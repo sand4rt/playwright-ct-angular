685e0ccff87d88491e07dc26e333956a0eaf3688
sand4rt <info@mesander.com>
2023-03-01 17:13:30 +0100

    chore: formatting

--- ct-angular/src/components/button.component.ts
+++ ct-angular/src/components/button.component.ts
@@ -2,8 +2,8 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   standalone: true,
+  selector: 'button-component',
   template: `<button (click)="submit.emit('hello')">{{title}}</button>`,
-  selector: 'button-component'
 })
 export class ButtonComponent {
   @Input() title!: string;
