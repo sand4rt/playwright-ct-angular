69d0f076cea6abb548f53368917e065d77087aa8
sand4rt <info@mesander.com>
2023-03-02 13:38:43 +0100

    feat: vite compilation

--- ct-angular/playwright/index.ts
+++ ct-angular/playwright/index.ts
@@ -1,23 +1,12 @@
 import { beforeMount, afterMount } from '@sand4rt/experimental-ct-angular/hooks';
-import { provideRouter } from '@angular/router';
-import { ButtonComponent } from '@/components/button.component';
-import { routes } from '@/router';
-import '@/assets/styles.css';
+import '../src/assets/styles.css';
 
 export type HooksConfig = {
   routing?: boolean
 }
 
 beforeMount<HooksConfig>(async ({ hooksConfig, TestBed }) => {
-  // TestBed.configureTestingModule({
-  //   imports: [ButtonComponent],
-  // });
-
-  // if (hooksConfig?.routing) {
-  //   TestBed.configureTestingModule({
-  //     providers: [provideRouter(routes)]
-  //   })
-  // }
+  console.log('After mount');
 });
 
 afterMount<HooksConfig>(async () => {
