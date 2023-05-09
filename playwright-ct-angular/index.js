c5ebd995db21edcbd1184bcd51224c4a01beeed7
sand4rt <info@mesander.com>
2023-05-09 19:56:53 +0200

    chore: bump vite-plugin-angular to 0.2.0-beta.6

--- playwright-ct-angular/index.js
+++ playwright-ct-angular/index.js
@@ -25,7 +25,7 @@ function plugin() {
   const { createPlugin } = require('@playwright/experimental-ct-core/lib/vitePlugin');
   return createPlugin(
     path.join(__dirname, 'registerSource.mjs'),
-    () => import('@analogjs/vite-plugin-angular').then(plugin => plugin.default())
+    () => import('@analogjs/vite-plugin-angular').then(plugin => plugin.default({ jit: false }))
   )
 };
 
