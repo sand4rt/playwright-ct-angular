667dd46eba74a28c485f3d639d4bda3c6b628095
sand4rt <info@mesander.com>
2023-08-26 23:43:14 +0200

    fix: plugin.default is not a function

--- playwright-ct-angular/index.js
+++ playwright-ct-angular/index.js
@@ -24,7 +24,12 @@ function plugin() {
   const { createPlugin } = require('@playwright/experimental-ct-core/lib/vitePlugin');
   return createPlugin(
     path.join(__dirname, 'registerSource.mjs'),
-    () => import('@analogjs/vite-plugin-angular').then(plugin => plugin.default({ jit: false }))
+    () => import('@analogjs/vite-plugin-angular').then(plugin => {
+      // TODO: remove the typeof plugin.default check
+      if (typeof plugin.default === 'function')
+        return plugin.default({ jit: false })
+      return plugin.default.default({ jit: false })
+    })
   )
 };
 
