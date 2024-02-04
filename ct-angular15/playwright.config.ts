1fdfae5194ebfc05f6b095466c05aeb8e3e54b70
sand4rt <info@mesander.com>
2024-02-04 16:49:19 +0100

    test: latest angular project

---
+++ ct-angular15/playwright.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig, devices } from '@sand4rt/experimental-ct-angular';
+import { resolve } from 'path';
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: 'tests',
+  forbidOnly: !!process.env['CI'],
+  retries: process.env['CI'] ? 2 : 0,
+  reporter: process.env['CI'] ? 'html' : 'line',
+  use: {
+    trace: 'on-first-retry',
+    ctViteConfig: {
+      resolve: {
+        alias: {
+          '@': resolve('./src'),
+        }
+      }
+    }
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+});
